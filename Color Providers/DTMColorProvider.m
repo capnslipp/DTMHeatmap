e8471e3021418972ad50c3ef9401c72d2bce7fd0
Onur Ersel <onurersel@gmail.com>
2017-09-28 10:24:59 +0200

    Optimization

--- Color Providers/DTMColorProvider.m
+++ Color Providers/DTMColorProvider.m
@@ -9,6 +9,19 @@
 #import "DTMColorProvider.h"
 #import <AHEasing/easing.h>
 
+int pointCount = 5;
+
+double colors[5][3] = {
+    {38, 210, 35},
+    {38, 210, 35},
+    {51, 232, 136},
+    {14, 226, 10},
+    {32, 244, 21},
+};
+
+double points[5] = {0.0, 0.2, 0.4, 0.8, 1.0};
+double opacities[5] = {0.0, 0.8, 1, 1, 1};
+
 @implementation DTMColorProvider
 
 - (void)colorForValue:(double)value
@@ -18,18 +31,40 @@
                 alpha:(CGFloat *)alpha
 {
     value = MIN(1, MAX(0, value));
-    //value = CubicEaseOut(value);
+    if (value > 1.0) {
+        value = 1.0;
+    } else if (value < 0.0) {
+        value = 0.0;
+    }
+
+    // getting index for low
+    int index = 0;
+    while (index < pointCount && points[index] < value) {
+        index++;
+    }
+    index--;
+
+    // getting points
+    double pLow = points[index];
+    double pHight = points[index+1];
+
+    // getting color ratios
+    double color2Ratio = (value - pLow) / (pHight - pLow);
+    double color1Ratio = 1 - color2Ratio;
+
+    double opacity = (opacities[index] * color1Ratio) + (opacities[index+1] * color2Ratio);
+
+    *red = (colors[index][0] * color1Ratio) + (colors[index+1][0] * color2Ratio);
+    *green = (colors[index][1] * color1Ratio) + (colors[index+1][1] * color2Ratio);
+    *blue = (colors[index][2] * color1Ratio) + (colors[index+1][2] * color2Ratio);
+
+    *red *= opacity;
+    *green *= opacity;
+    *blue *= opacity;
+    *alpha = opacity * 255.0;
 
     /*
-    NSArray *colors = @[
-                        @[@(73), @(113), @(71)],
-                        @[@(73), @(113), @(71)],
-                        @[@(33), @(92), @(32)],
-                        @[@(34), @(140), @(62)],
-                        @[@(39), @(223), @(36)],
-                        ];
-     
-     */
+    //value = CubicEaseOut(value);
 
     NSArray *colors = @[
                         @[@(38), @(210), @(35)],
@@ -85,7 +120,7 @@
     *green *= opacity;
     *blue *= opacity;
     *alpha = opacity * 255.0;
-
+     */
 }
 
 @end
--- DTMHeatmapRenderer.m
+++ DTMHeatmapRenderer.m
@@ -67,104 +67,108 @@ static const NSInteger kSBHeatRadiusInPoints = 100;
     MKMapRect visibleRect = [self.overlay boundingMapRect];
     MKMapRect mapIntersect = MKMapRectIntersection(mapRect, visibleRect);
     CGRect usIntersect = [self rectForMapRect:mapIntersect]; //rect in user space coordinates (NOTE: not in screen points)
-    
+
     int columns = ceil(CGRectGetWidth(usRect) * zoomScale);
     int rows = ceil(CGRectGetHeight(usRect) * zoomScale);
     int arrayLen = columns * rows;
-    
+
     // allocate an array matching the screen point size of the rect
     float *pointValues = calloc(arrayLen, sizeof(float));
-   
-    if (pointValues) {
-        // pad out the mapRect with the radius on all sides.
-        // we care about points that are not in (but close to) this rect
-        CGRect paddedRect = [self rectForMapRect:mapRect];
-        paddedRect.origin.x -= kSBHeatRadiusInPoints / zoomScale;
-        paddedRect.origin.y -= kSBHeatRadiusInPoints / zoomScale;
-        paddedRect.size.width += 2 * kSBHeatRadiusInPoints / zoomScale;
-        paddedRect.size.height += 2 * kSBHeatRadiusInPoints / zoomScale;
-        MKMapRect paddedMapRect = [self mapRectForRect:paddedRect];
-        
-        // Get the dictionary of values out of the model for this mapRect and zoomScale.
-        DTMHeatmap *hm = (DTMHeatmap *)self.overlay;
-        NSDictionary *heat = [hm mapPointsWithHeatInMapRect:paddedMapRect
-                                                    atScale:zoomScale];
-        
-        for (NSValue *key in heat) {
-            // convert key to mapPoint
-            MKMapPoint mapPoint;
-            [key getValue:&mapPoint];
-            double value = [[heat objectForKey:key] doubleValue];
-            
-            // figure out the correspoinding array index
-            CGPoint usPoint = [self pointForMapPoint:mapPoint];
-            
-            CGPoint matrixCoord = CGPointMake((usPoint.x - usRect.origin.x) * zoomScale,
-                                              (usPoint.y - usRect.origin.y) * zoomScale);
-            
-            if (value != 0 && !isnan(value)) { // don't bother with 0 or NaN
-                // iterate through surrounding pixels and increase
-                for (int i = 0; i < 2 * kSBHeatRadiusInPoints; i++) {
-                    for (int j = 0; j < 2 * kSBHeatRadiusInPoints; j++) {
-                        // find the array index
-                        int column = floor(matrixCoord.x - kSBHeatRadiusInPoints + i);
-                        int row = floor(matrixCoord.y - kSBHeatRadiusInPoints + j);
-                        
-                        // make sure this is a valid array index
-                        if (row >= 0 && column >= 0 && row < rows && column < columns) {
-                            int index = columns * row + column;
-                            double addVal = value * _scaleMatrix[j * 2 * kSBHeatRadiusInPoints + i];
-                            pointValues[index] += addVal;
-                        }
+
+    if (!pointValues) {
+        return;
+    }
+
+    // pad out the mapRect with the radius on all sides.
+    // we care about points that are not in (but close to) this rect
+    CGRect paddedRect = [self rectForMapRect:mapRect];
+    paddedRect.origin.x -= kSBHeatRadiusInPoints / zoomScale;
+    paddedRect.origin.y -= kSBHeatRadiusInPoints / zoomScale;
+    paddedRect.size.width += 2 * kSBHeatRadiusInPoints / zoomScale;
+    paddedRect.size.height += 2 * kSBHeatRadiusInPoints / zoomScale;
+    MKMapRect paddedMapRect = [self mapRectForRect:paddedRect];
+
+    // Get the dictionary of values out of the model for this mapRect and zoomScale.
+    DTMHeatmap *hm = (DTMHeatmap *)self.overlay;
+    NSDictionary *heat = [hm mapPointsWithHeatInMapRect:paddedMapRect
+                                                atScale:zoomScale];
+
+    for (NSValue *key in heat) {
+        // convert key to mapPoint
+        MKMapPoint mapPoint;
+        [key getValue:&mapPoint];
+        double value = [[heat objectForKey:key] doubleValue];
+
+        // figure out the correspoinding array index
+        CGPoint usPoint = [self pointForMapPoint:mapPoint];
+
+        CGPoint matrixCoord = CGPointMake((usPoint.x - usRect.origin.x) * zoomScale,
+                                          (usPoint.y - usRect.origin.y) * zoomScale);
+
+        if (value != 0 && !isnan(value)) { // don't bother with 0 or NaN
+            // iterate through surrounding pixels and increase
+            for (int i = 0; i < 2 * kSBHeatRadiusInPoints; i++) {
+                for (int j = 0; j < 2 * kSBHeatRadiusInPoints; j++) {
+                    // find the array index
+                    int column = floor(matrixCoord.x - kSBHeatRadiusInPoints + i);
+                    int row = floor(matrixCoord.y - kSBHeatRadiusInPoints + j);
+
+                    // make sure this is a valid array index
+                    if (row >= 0 && column >= 0 && row < rows && column < columns) {
+                        int index = columns * row + column;
+                        double addVal = value * _scaleMatrix[j * 2 * kSBHeatRadiusInPoints + i];
+                        pointValues[index] += addVal;
                     }
                 }
             }
         }
-        
-        CGFloat red, green, blue, alpha;
-        uint indexOrigin;
-        unsigned char *rgba = (unsigned char *)calloc(arrayLen * 4, sizeof(unsigned char));
-        DTMColorProvider *colorProvider = [hm colorProvider];
-        for (int i = 0; i < arrayLen; i++) {
-            if (pointValues[i] != 0) {
-                indexOrigin = 4 * i;
-                [colorProvider colorForValue:pointValues[i]
-                                         red:&red
-                                       green:&green
-                                        blue:&blue
-                                       alpha:&alpha];
-                
-                rgba[indexOrigin] = red;
-                rgba[indexOrigin + 1] = green;
-                rgba[indexOrigin + 2] = blue;
-                rgba[indexOrigin + 3] = alpha;
-            }
-        }
-        
-        free(pointValues);
-
-        CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-        CGContextRef bitmapContext = CGBitmapContextCreate(rgba,
-                                                           columns,
-                                                           rows,
-                                                           8, // bitsPerComponent
-                                                           4 * columns, // bytesPerRow
-                                                           colorSpace,
-                                                           kCGImageAlphaPremultipliedLast | kCGBitmapByteOrderDefault);
-
-       
-        CGImageRef cgImage = CGBitmapContextCreateImage(bitmapContext);
-        UIImage *img = [UIImage imageWithCGImage:cgImage];
-        UIGraphicsPushContext(context);
-        //[img drawInRect:usIntersect];
-        [img drawInRect:usIntersect blendMode:kCGBlendModeNormal alpha:0.4];
-        UIGraphicsPopContext();
-        
-        CFRelease(cgImage);
-        CFRelease(bitmapContext);
-        CFRelease(colorSpace);
-        free(rgba);
     }
+
+    CGFloat red, green, blue, alpha;
+    uint indexOrigin;
+    unsigned char *rgba = (unsigned char *)calloc(arrayLen * 4, sizeof(unsigned char));
+    DTMColorProvider *colorProvider = [hm colorProvider];
+
+    for (int i = 0; i < arrayLen; i++) {
+        if (pointValues[i] != 0) {
+            indexOrigin = 4 * i;
+            [colorProvider colorForValue:pointValues[i]
+                                     red:&red
+                                   green:&green
+                                    blue:&blue
+                                   alpha:&alpha];
+
+            rgba[indexOrigin] = red;
+            rgba[indexOrigin + 1] = green;
+            rgba[indexOrigin + 2] = blue;
+            rgba[indexOrigin + 3] = alpha;
+        }
+    }
+
+    free(pointValues);
+
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    CGContextRef bitmapContext = CGBitmapContextCreate(rgba,
+                                                       columns,
+                                                       rows,
+                                                       8, // bitsPerComponent
+                                                       4 * columns, // bytesPerRow
+                                                       colorSpace,
+                                                       kCGImageAlphaPremultipliedLast | kCGBitmapByteOrderDefault);
+
+
+    CGImageRef cgImage = CGBitmapContextCreateImage(bitmapContext);
+    UIImage *img = [UIImage imageWithCGImage:cgImage];
+    UIGraphicsPushContext(context);
+    //[img drawInRect:usIntersect];
+    [img drawInRect:usIntersect blendMode:kCGBlendModeNormal alpha:0.4];
+    UIGraphicsPopContext();
+
+    CFRelease(cgImage);
+    CFRelease(bitmapContext);
+    CFRelease(colorSpace);
+    free(rgba);
+
 }
 
 @end
