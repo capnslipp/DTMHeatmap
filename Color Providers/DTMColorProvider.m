4f961ef402b4b911ba675ee7f462ac12726b1b45
Slipp Douglas Thompson <git@capnslipp.com>
2023-03-21 10:02:44 -0500

    Revert `DTMHeatmapRenderer.m` from "New colors"
    This reverts commit 4344cbc68ea2cbdbb52e5321a5deb8a43dcad586.

--- Color Providers/DTMColorProvider.m
+++ Color Providers/DTMColorProvider.m
@@ -7,26 +7,6 @@
 //
 
 #import "DTMColorProvider.h"
-#import <AHEasing/easing.h>
-
-int pointCount = 4;
-
-double colors[4][3] = {
-    /*
-    Simon Broström [2:55 PM]
-    ‘rgba(119, 27, 189, 0)’,
-    ‘rgba(18, 157, 218, 1)’,
-    ‘rgba(0, 255, 201, 1)’,
-    ‘rgba(106, 252, 156, 1)’,
-    */
-    {119, 27, 189},
-    {18, 157, 218},
-    {0, 255, 201},
-    {106, 252, 156},
-};
-
-double points[4] = {0.0, 0.33, 0.66, 1.0};
-double opacities[4] = {0.0, 1, 1, 1};
 
 @implementation DTMColorProvider
 
@@ -36,39 +16,35 @@ double opacities[4] = {0.0, 1, 1, 1};
                  blue:(CGFloat *)blue
                 alpha:(CGFloat *)alpha
 {
+    static int maxVal = 255;
+    static double maxOpacity = 0.7;
+
     value = MIN(1, MAX(0, value));
-    if (value > 1.0) {
-        value = 1.0;
-    } else if (value < 0.0) {
-        value = 0.0;
+    value = sqrt(value);
+
+
+    double a = value * 9;
+    a = MIN(maxOpacity, MAX(0, a));
+
+    static double p1 = 0.17;
+    if (value < p1) {
+        double w1 = value / p1;
+        double w2 = 1 - w1;
+
+        *red = (50 * w1) + (0 * w2);
+        *green = (150 * w1) + (0 * w2);
+        *blue = (50 * w1) + (120 * w2);
+    } else {
+        double w1 = (value-p1) / (1-p1);
+        double w2 = 1 - w1;
+
+        *red = (102 * w1) + (50 * w2);
+        *green = (225 * w1) + (150 * w2);
+        *blue = (0 * w1) + (50 * w2);
     }
 
-    // getting index for low
-    int index = 0;
-    while (index < pointCount && points[index] < value) {
-        index++;
-    }
-    index--;
+    *alpha = a * maxVal;
 
-    // getting points
-    double pLow = points[index];
-    double pHight = points[index+1];
-
-    // getting color ratios
-    double color2Ratio = (value - pLow) / (pHight - pLow);
-    double color1Ratio = 1 - color2Ratio;
-
-    double opacity = (opacities[index] * color1Ratio) + (opacities[index+1] * color2Ratio);
-
-    *red = (colors[index][0] * color1Ratio) + (colors[index+1][0] * color2Ratio);
-    *green = (colors[index][1] * color1Ratio) + (colors[index+1][1] * color2Ratio);
-    *blue = (colors[index][2] * color1Ratio) + (colors[index+1][2] * color2Ratio);
-
-    *red *= opacity;
-    *green *= opacity;
-    *blue *= opacity;
-    *alpha = opacity * 255.0;
 }
 
 @end
-
